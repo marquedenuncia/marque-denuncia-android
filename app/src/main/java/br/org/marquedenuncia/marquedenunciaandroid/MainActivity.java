57dddf2390ce3a338278f9003505833199909867
oliveirabruno <bruno_araujo15@hotmail.com>
2015-11-29 16:05:55 -0300

    Added initial configuration using google maps API

--- app/src/main/java/br/org/marquedenuncia/marquedenunciaandroid/MainActivity.java
+++ app/src/main/java/br/org/marquedenuncia/marquedenunciaandroid/MainActivity.java
@@ -1,15 +1,35 @@
 package br.org.marquedenuncia.marquedenunciaandroid;
 
+import android.location.Location;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMapOptions;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener{
+    private SupportMapFragment mapFrag;
+    private GoogleMap map;
+    private Marker marker;
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +38,86 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        GoogleMapOptions options = new GoogleMapOptions();
+        options.zOrderOnTop(true);
+
+        mapFrag = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.frag1);
+        map = mapFrag.getMap();
+
+        configMap();
+    }
+
+    public void configMap(){
+        map = mapFrag.getMap();
+        map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        map.getUiSettings().setZoomControlsEnabled(true);
+
+        LatLng latLng = new LatLng(-12.8809348,-38.4175853);
+
+        CameraPosition cameraPosition = new CameraPosition.Builder().target(latLng).zoom(11).bearing(0).tilt(90).build();
+        CameraUpdate update = CameraUpdateFactory.newCameraPosition(cameraPosition);
+
+        //map.moveCamera(update);
+        map.animateCamera(update, 3000, new GoogleMap.CancelableCallback() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onFinish() {
+            }
+
+            @Override
+            public void onCancel() {
             }
         });
+
+        //Events
+        map.setOnCameraChangeListener(new GoogleMap.OnCameraChangeListener() {
+            @Override
+            public void onCameraChange(CameraPosition cameraPosition) {
+                if (marker != null) {
+                    marker.remove();
+                }
+            }
+        });
+        callConnectionGPS();
+    }
+
+    public void customAddMarker(LatLng latLng, String title, String snipper){
+        MarkerOptions options = new MarkerOptions();
+        options.position(latLng).title(title).snippet(snipper).draggable(true);
+
+        Marker marker =  map.addMarker(options);
+    }
+
+
+    private synchronized void callConnectionGPS(){
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addOnConnectionFailedListener(this)
+                .addConnectionCallbacks(this)
+                .addApi(LocationServices.API)
+                .build();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Location l = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+
+        if(l != null){
+
+            customAddMarker(new LatLng(l.getLatitude(),l.getLongitude()), "My Location", "");
+        }
+
+    }
+
+
+    //Implementaçao Default
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
     }
 
     @Override
